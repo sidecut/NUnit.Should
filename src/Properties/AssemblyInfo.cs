0ebf4091537fe756d24b18c7ef035fc0df2bc69b
James Kelly <james.kelly.99@gmail.com>
2011-05-19 12:03:55 +1000

    Create .NET 4.0 projects for specs and src

---
+++ src/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("NUnit.Should")]
+[assembly: AssemblyDescription("Extension methods that allow you to write NUnit tests using a more BDD .Should() syntax")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("remi Taylor")]
+[assembly: AssemblyProduct("NUnit.Should")]
+[assembly: AssemblyCopyright("Copyright © 2010")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("eb5d7069-3566-46c4-9145-0a643b938ad0")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.2")]
+[assembly: AssemblyFileVersion("1.0.2")]
